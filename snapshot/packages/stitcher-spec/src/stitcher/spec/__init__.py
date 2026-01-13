87f01434cd9602148474571a42de1fc80db1deb8
doucx <doucxldh@gmail.com>
2026-01-13 17:32:40 +0800

    Capture: M __init__.py
    X-Quipu-Output-Tree: 5283d6f5572e4797b1d3a59a1442130c0b470d0d

--- snapshot/packages/stitcher-spec/src/stitcher/spec/__init__.py
+++ snapshot/packages/stitcher-spec/src/stitcher/spec/__init__.py
@@ -30,7 +30,7 @@ from .protocols import (
     URIGeneratorProtocol,
 )
 from .storage import IndexStoreProtocol
-from .managers import DocumentManagerProtocol, SignatureManagerProtocol
+from .managers import DocumentManagerProtocol
 
 __all__ = [
     "DocstringIR",
